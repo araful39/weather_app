4be766010a61a885732b7b1cc8e14399ce8596a0
Md Araful islam Raju <143991599+araful39@users.noreply.github.com>
2024-09-27 18:44:02 +0600

    temperature add

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -8,32 +8,127 @@ class Home extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Container(
-          color: const Color(0xff97ABFF),
-          child: const Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              colors: [
+                Color(0xFF97ABFF),
+                Color(0xFF121397),
+              ],
+              begin: FractionalOffset(0.0, 0.0),
+              end: FractionalOffset(1.0, 0.0),
+              stops: [0.0, 1.0],
+              tileMode: TileMode.clamp),
+
+
+        ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Text(
+              const Text(
                 "Dhaka",
                 style: TextStyle(
                     color: Colors.white,
                     fontSize: 30,
                     fontWeight: FontWeight.bold),
               ),
-              Row(
+              const Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Icon(
                     Icons.location_on_sharp,
-                    color:Colors.white,
-                  ),Text("Current Location",style: TextStyle(color: Colors.white),)
+                    color: Colors.white,
+                  ),
+                  Text(
+                    "Current Location",
+                    style: TextStyle(color: Colors.white),
+                  )
                 ],
               ),
               Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image.asset("assets/cloudy.png"),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        "13",
+                        style: TextStyle(
+                            color: Colors.white,
+
+                            fontSize: 80),
+                      ),
+                      Icon(
+                        Icons.circle_outlined,
+                        color: Colors.white,
+                        size: 30,
+                      )
+                    ],
+                  )
+                ],
+              ),
+             
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
 
+                  Row(
+                    children: [
+                      Text(
+                        "Partly Cloud -",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "H:10",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Icon(
+                            Icons.circle_outlined,
+                            color: Colors.white,
+                            size: 5,
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "L:4",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Icon(
+                            Icons.circle_outlined,
+                            color: Colors.white,
+                            size: 5,
+                          )
+                        ],
+                      ),
+                    ],
+                  )
                 ],
-              )
+              ),
             ],
           ),
         ),
